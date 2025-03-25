67481fddf38b63e3186cd81f9bd2cd511599e3d4
unknown <maazratlam5@gmail.com>
2025-03-26 03:17:01 +0530

    small bug fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import ComboBox from './Combobox'
+import ComboBox from './ComBoBox'
 
 function App() {
   return (
